e25d7731379d493be071c3840db3596f412ee111
WM Chang <wandererm@gmail.com>
2014-03-14 18:57:19 +0800

    add more bricks

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -19,9 +19,21 @@ function update() {
 		document.querySelector('#header .top2').style.transform = 'translateY(' + currentScrollY * 0.5 + 'px)';
 	}
 	var footer = document.getElementById('footer');
-	if (latestKnownScrollY + document.documentElement.clientHeight > footer.offsetTop) {
-		var offset = -30 + (footer.clientHeight - (latestKnownScrollY + document.documentElement.clientHeight - footer.offsetTop)) * 0.5;
+	if (currentScrollY + document.documentElement.clientHeight > footer.offsetTop) {
+		var offset = -30 + (footer.clientHeight - (currentScrollY + document.documentElement.clientHeight - footer.offsetTop)) * 0.5;
 		footer.style.backgroundPosition = 'center bottom ' + offset + 'px';
+	}
+	if (currentScrollY > 30 && currentScrollY < 150) {
+		document.querySelector('.pillar').style.transform = 'translateY(' + ((currentScrollY - 30) * 0.5 - 20) + 'px)';
+	}
+	if (currentScrollY > 150 && currentScrollY < 280) {
+		document.querySelector('.pillar2').style.transform = 'translateY(' + ((currentScrollY - 150) * 0.7 - 30) + 'px)';
+	}
+	if (currentScrollY > 280 && currentScrollY < 380) {
+		//document.querySelector('.tap').style.transform = 'translateY(' + ((currentScrollY - 250)) + 'px)';
+	}
+	if (currentScrollY > 380) {
+		document.querySelector('.drop').classList.add('start');
 	}
 }
 window.addEventListener('scroll', onScroll, false);
